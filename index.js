948a4cedfca93166134dd4c0ab16d5a9c7001af9
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-01-15 22:12:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (){
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const  createFareMultiplier = function(themultiplier){
+    return function(themultiplied){
+        return themultiplied * themultiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+
+
+const selectDifferentDrivers = function (drivers, driversToReturn) {
+    return driversToReturn(drivers);
+  };
